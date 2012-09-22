2351c32a4b76c4119c82957689ae74bb8fd5f482
Jan Teske <jan.teske@gmail.com>
2012-09-22 21:13:34 +0200

    added rememberme functionality

--- WettApp/WettApp/users/urls.py
+++ WettApp/WettApp/users/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import logout
 from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('WettApp.users.views',
-    url(ur'^login/$', login, {'template_name': 'users/login.html'},
-        name='login'),
+    url(ur'^login/$', 'login', name='login'),
     url(ur'^logout/$', logout, {'next_page': reverse_lazy('login')},
         name='logout'),
 )
--- WettApp/WettApp/users/views.py
+++ WettApp/WettApp/users/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.contrib.auth import views as authviews
+
+from WettApp.users.forms import LoginForm
+
+
+def login(request):
+    if request.method == 'POST' and not request.POST.get('remember_me', None):
+        request.session.set_expiry(0)
+    return authviews.login(request, template_name='users/login.html',
+                           authentication_form=LoginForm)
