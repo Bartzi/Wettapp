f31b226c77dc2951fc08d3032c14f16462ac8e82
Jan Teske <jan.teske@gmail.com>
2012-09-22 22:53:07 +0200

    added first buddy list

--- WettApp/WettApp/users/urls.py
+++ WettApp/WettApp/users/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.contrib.auth.views import logout
 from django.core.urlresolvers import reverse_lazy
 
@@ -8,4 +8,5 @@ urlpatterns = patterns('WettApp.users.views',
     url(ur'^login/$', 'login', name='login'),
     url(ur'^logout/$', logout, {'next_page': reverse_lazy('login')},
         name='logout'),
+    url(ur'^buddies/$', 'buddy_list', name='buddy-list'),
 )
