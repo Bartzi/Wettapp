f1b6a772f2396f78b0c2f75db5749982b8d3a5cc
Jan Teske <jan.teske@gmail.com>
2012-09-22 21:55:06 +0200

    added forms file

---
+++ WettApp/WettApp/users/forms.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):
+    remember_me = forms.BooleanField(required=False)
