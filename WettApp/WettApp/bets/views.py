26f6e4cc6cc64bfbecb5182faaa6e1c715f4eefc
Christian Bartz <christianbartz@gmail.com>
2012-09-22 19:09:07 +0200

    first implementation for bet overview view bla

--- WettApp/WettApp/bets/views.py
+++ WettApp/WettApp/bets/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from bets.models import Bet
 
 
 @login_required
 def index(request):
-    return render(request, 'bets/index.html')
+    user_bets = Bet.objects.filter(participants__contains=request.user)
+    return render(request, 'bets/index.html', {'bets': user_bets})
