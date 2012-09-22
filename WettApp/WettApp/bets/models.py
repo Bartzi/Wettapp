35238c5cce09e8f47953ddfd286744fb655ae84e
Christian Bartz <christianbartz@gmail.com>
2012-09-22 21:37:34 +0200

    made changes in bets app
    we now have models for bets and scores for bets (see models.py)
    we also have a bet-overview (see views.py) --> lateron also
    /bets/index.html
    you can add bets via the admin

--- WettApp/WettApp/bets/models.py
+++ WettApp/WettApp/bets/models.py
@@ -11,8 +11,17 @@ class Bet(models.Model):
     start_date = models.DateTimeField('start Date')
     participants = models.ManyToManyField(User)
 
+    def participant_score(self, participant):
+        return self.bet_scores.get(user=participant)
+
+    def __unicode__(self):
+        return self.title
+
 
 class BetScore(models.Model):
     score = models.IntegerField()
     user = models.ForeignKey(User, related_name='bet_scores')
     bet = models.ForeignKey(Bet, related_name='bet_scores')
+
+    def __unicode__(self):
+        return unicode(self.score)
--- WettApp/WettApp/bets/views.py
+++ WettApp/WettApp/bets/views.py
@@ -8,4 +8,13 @@ from WettApp.bets.models import Bet
 @login_required
 def index(request):
     user_bets = Bet.objects.filter(participants__in=[request.user])
-    return render(request, 'bets/index.html', {'bets': user_bets})
+    bet_list = []
+    for bet in user_bets:
+        scores_list = []
+        for participant in bet.participants.all():
+            score = bet.participant_score(participant)
+            scores_list.append((participant, score))
+        bet_tuple = (bet, scores_list)
+        bet_list.append(bet_tuple)
+
+    return render(request, 'bets/index.html', {'bet_list': bet_list})
