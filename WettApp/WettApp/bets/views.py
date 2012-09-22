c071f2838751a71c87e39af7ade941c9e3b9a2d2
Christian Bartz <christianbartz@gmail.com>
2012-09-22 19:13:20 +0200

    index view works for now

--- WettApp/WettApp/bets/views.py
+++ WettApp/WettApp/bets/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from bets.models import Bet
+from WettApp.bets.models import Bet
 
 
 @login_required
 def index(request):
-    user_bets = Bet.objects.filter(participants__contains=request.user)
+    user_bets = Bet.objects.filter(participants__in=[request.user])
     return render(request, 'bets/index.html', {'bets': user_bets})
