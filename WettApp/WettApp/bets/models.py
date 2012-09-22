7b9f1bca553fb8ed65699f541d6d42a14fcf5e28
Christian Bartz <christianbartz@gmail.com>
2012-09-22 18:44:16 +0200

    added some models for bets

--- WettApp/WettApp/bets/models.py
+++ WettApp/WettApp/bets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Bet(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    startDate = models.DateTimeField('start Date')
+    participants = models.ManyToManyField(User)
+
+
+class BetScore(models.Model):
+    score = models.IntegerField()
+    user = models.ForeignKey(User)
+    bet = models.ForeignKey(Bet)
