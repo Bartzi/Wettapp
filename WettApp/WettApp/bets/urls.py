425c1982e6d4ec4ed2186f3ecf571c74c91bfcb4
Jan Teske <jan.teske@gmail.com>
2012-09-22 18:10:17 +0200

    added bets app

---
+++ WettApp/WettApp/bets/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
+
+urlpatterns = patterns('WettApp.bets.views',
+    url(ur'^$', RedirectView.as_view(url=u'index/'), name='root-bets'),
+    url(ur'^index/$', 'index', name='login'),
+)
--- WettApp/WettApp/urls.py
+++ WettApp/WettApp/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(ur'^$', RedirectView.as_view(url=u'bets/'), name='root'),
+    url(r'^bets/', include('WettApp.bets.urls')),
     url(r'^users/', include('WettApp.users.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
