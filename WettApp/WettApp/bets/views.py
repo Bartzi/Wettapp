592de72f2bde585c8e183b89a1b94adfc0547c38
Christian Bartz <christianbartz@gmail.com>
2012-09-22 23:14:26 +0200

    added functionality to color divs differently

--- WettApp/WettApp/bets/views.py
+++ WettApp/WettApp/bets/views.py
@@ -14,7 +14,17 @@ def index(request):
         for participant in bet.participants.all():
             score = bet.participant_score(participant)
             scores_list.append((participant, score))
-        bet_tuple = (bet, scores_list)
+        score_diff = scores_list[0][1].score - scores_list[1][1].score
+        if request.user == scores_list[0][0]:
+            if score_diff < 0:
+                bet_tuple = (bet, scores_list, "warning")
+            else:
+                bet_tuple = (bet, scores_list, "good")
+        else:
+            if score_diff > 0:
+                bet_tuple = (bet, scores_list, "good")
+            else:
+                bet_tuple = (bet, scores_list, "warning")
         bet_list.append(bet_tuple)
 
     return render(request, 'bets/index.html', {'bet_list': bet_list})
