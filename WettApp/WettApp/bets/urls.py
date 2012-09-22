c9fb8aea0c3565e5c742ebdac04da9e10149c7bf
Jan Teske <jan.teske@gmail.com>
2012-09-22 19:37:06 +0200

    added navbar links

--- WettApp/WettApp/bets/urls.py
+++ WettApp/WettApp/bets/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('WettApp.bets.views',
     url(ur'^$', RedirectView.as_view(url=u'index/'), name='root-bets'),
-    url(ur'^index/$', 'index', name='login'),
+    url(ur'^index/$', 'index', name='index-bets'),
 )
